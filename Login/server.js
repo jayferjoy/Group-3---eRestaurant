731316e957913d3bab166470c07f90d58fda66eb
jayferjoy <jayfer.development@gmail.com>
2021-09-07 16:29:47 +1000

    Update server.js

--- Login/server.js
+++ Login/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
-const app= express(); 
-app.use('express.json()');
+const app = express(); 
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello Coders!')
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 const PORT = 8080;
 
 app.listen(PORT, () => {
-    console.log("Your application is running in ${PORT}")
+    console.log('Your application is running in %d', PORT )
 });
 
 
