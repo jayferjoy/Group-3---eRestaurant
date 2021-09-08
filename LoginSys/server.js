4ea507e2ff49f5c885e0cac128fd6ae85dbbc880
L-Sherratt <62371728+L-Sherratt@users.noreply.github.com>
2021-09-08 16:21:50 +1000

    Update server.js

--- LoginSys/server.js
+++ LoginSys/server.js
@@ -12,5 +12,11 @@ app.use('/api/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log (`Server running on port ${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`Sever running on port ${PORT}`)
+);
 
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Logged Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
