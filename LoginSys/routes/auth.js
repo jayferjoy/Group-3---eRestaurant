470ed6775ded6911698e7bc4f62409ce9ea6c653
jayferjoy <jayfer.development@gmail.com>
2021-09-08 17:19:43 +1000

    Changes to resetpassword route

--- LoginSys/routes/auth.js
+++ LoginSys/routes/auth.js
@@ -4,15 +4,13 @@ const router = express.Router();
 
 const {register, login, forgotpassword, resetpassword} = require ('../controllers/auth');
 
-
 router.route("/register").post(register);
-
 
 router.route("/login").post(login);
 
 router.route("/forgotpassword").post(forgotpassword);
 
-router.route("/resetpassword/:resetToken").post(resetpassword);
+router.route("/resetpassword/:resetToken").put(resetpassword);
 
 module.exports = router;
 
