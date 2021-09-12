4a8eb2f1e18bfa02c6da087933de352e3a7b7150
JayCod3r <84632172+JayCod3r@users.noreply.github.com>
2021-09-12 13:20:33 +1000

    home page, buttons
    Added styling home page, buttons

--- LoginSys/client/src/App.js
+++ LoginSys/client/src/App.js
@@ -1,7 +1,37 @@
+import {Link} from "react-router-dom";
+import'./App.css';
+import axios from 'axios';
+
 const App =() => {
   return (
     <div className="App">
-      React App
+   
+
+      <div className="div1">
+        <h1 >Le Bistrot d’Andre</h1>
+
+      </div>
+
+      <div style={{ 
+        backgroundImage: `url("https://i.imgur.com/UH3w8wg.jpg")` 
+      }}> 
+      </div>
+
+      <button class="button1">Register</button>
+      <button class="button1">Login</button>
+      <button class="button1">Menu</button>
+      <button class="button1">Booking</button>
+
+
+
+      <div><img 
+      src="https://i.imgur.com/UH3w8wg.jpg"
+      alt="new"
+      />
+      </div>
+      
+
+
     </div>
   );
 }
