cb5cdfbbb6e71c7400010cee46882b279d78a82e
JayCod3r <84632172+JayCod3r@users.noreply.github.com>
2021-09-13 01:23:47 +1000

    adding routes
    Routing partially added

--- LoginSys/client/src/App.js
+++ LoginSys/client/src/App.js
@@ -1,38 +1,22 @@
-import {Link} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import'./App.css';
 import axios from 'axios';
 
+
 const App =() => {
   return (
-    <div className="App">
-   
 
-      <div className="div1">
-        <h1 >Le Bistrot d’Andre</h1>
-
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route exact path="/login" component={LoginScreen}/>
+          <Route exact path="/register" component={RegisterScreen}/>
+          <Route exact path="/forgotpassword" component={ForgotPassword}/>
+          <Route exact path="/passwordreset" component={PasswordReset}/>
+        </Switch>
       </div>
+    </Router>
 
-      <div style={{ 
-        backgroundImage: `url("https://i.imgur.com/UH3w8wg.jpg")` 
-      }}> 
-      </div>
-
-      <button class="button1">Register</button>
-      <button class="button1">Login</button>
-      <button class="button1">Menu</button>
-      <button class="button1">Booking</button>
-
-
-
-      <div><img 
-      src="https://i.imgur.com/UH3w8wg.jpg"
-      alt="new"
-      />
-      </div>
-      
-
-
-    </div>
   );
 }
 
--- LoginSys/client/src/components/Screens/LoginScreen.js
+++ LoginSys/client/src/components/Screens/LoginScreen.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import'./App.css';
+
+
 
 function Login() {
   return (
     <div>    
-    <h1>Login</h1>
+    <h1 >Login</h1>
     
     </div>
 
