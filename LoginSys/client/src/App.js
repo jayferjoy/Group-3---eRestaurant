7d20975983f00b0a6feca52af3233864b6c710ca
JayCod3r <84632172+JayCod3r@users.noreply.github.com>
2021-09-13 01:31:20 +1000

    Update App.js
    added screens/pages

--- LoginSys/client/src/App.js
+++ LoginSys/client/src/App.js
@@ -2,6 +2,13 @@ import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import'./App.css';
 import axios from 'axios';
 
+import PrivateRoute from './components/routing/PrivateRoute';
+
+import PrivateScreen from "./components/screens/PrivateScreen";
+import LoginScreen from "./components/screens/LoginScreen";
+import RegisterScreen from "./components/screens/RegisterScreen";
+import ForgotPasswordScreen from "./components/screens/ForgotPasswordScreen";
+import ResetPasswordScreen from "./components/screens/ResetPasswordScreen";
 
 const App =() => {
   return (
@@ -9,10 +16,11 @@ const App =() => {
     <Router>
       <div className="app">
         <Switch>
+        <PrivateRoute exact path ="/" component={PrivateScreen}/>
           <Route exact path="/login" component={LoginScreen}/>
           <Route exact path="/register" component={RegisterScreen}/>
-          <Route exact path="/forgotpassword" component={ForgotPassword}/>
-          <Route exact path="/passwordreset" component={PasswordReset}/>
+          <Route exact path="/forgotpassword" component={ForgotPasswordScreen}/>
+          <Route exact path="/passwordreset/:resetToken" component={ResetPasswordScreen}/>
         </Switch>
       </div>
     </Router>
