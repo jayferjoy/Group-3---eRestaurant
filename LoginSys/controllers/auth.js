32e07fc41e9e1813e0438b622e7493e75fa87e16
L-Sherratt <62371728+L-Sherratt@users.noreply.github.com>
2021-09-13 15:28:46 +1000

    updating auth
    changes to reset/login/frontend destination

--- LoginSys/controllers/auth.js
+++ LoginSys/controllers/auth.js
@@ -1,83 +1,105 @@
-const User = require('../models/User');
-const ErrorResponse = require('../utlis/errorResponse');
+const User = require("../models/User");
+const ErrorResponse = require("../utils/errorResponse");
+const sendEmail = require('../utils/sendEmail');
 
 exports.register = async (req, res, next) => {
-    const {username, email, password} = req.body;
+    const { username, email, password } = req.body;
+  
+    try {
+      const user = await User.create({
+        username,
+        email,
+        password,
+      });
+  
+      sendToken(user, 200, res);
+    } catch (err) {
+      next(err);
+    }
+  };
+  
 
-    //error handling mechanism
-    try{
-        const user = await User.create({
-            username, 
-            email, 
-            password,
-        });
-        
-    res.status(201).json({
-        success: true,
-        token:"asdjklasdlasdkl"
-    });
- 
+exports.login = async (req, res, next) => {
+    const {email, password} = req.body;
+
+    if(!email || !password) {
+        return next(new ErrorResponse("Please provide an email and password", 400));
+
+    }
+    try {
+        const user = await User.findOne({ email }).select("+password");
+
+        if(!user) {
+            return next(new ErrorResponse("Invalid credentials", 401));
+        }
+
+
+        const isMatch = await user.matchPasswords(password);
+
+        if(!isMatch) {
+            return next(new ErrorResponse("Invalid credentials", 401));
+        }
+
+        sendToken(user, 200, res);
 
     } catch (error) {
-   
         next(error);
 
     }
-    
-};
+}
 
-
-
-exports.login =  async(req, res, next) => {
-    const {email, password} = req.body;
-
-
-
-    if(!email || !password){
-           return next (new ErrorResponse("Please provide an email and password",400));
-    }
+exports.forgotpassword = async (req, res, next) => {
+    const {email} = req.body;
 
     try {
-        // getting user from db , access's the users password
-        const user = await User.findOne({email}).select("+password");
+        const user = await User.findOne({email});
+
+        if(!user) {
+            return next(new ErrorResponse("Email could not be sent", 404))
+        }
+
+        const resetToken = user.getResetPasswordToken();
+
+        await user.save();
+
+        const resetURL = `http://localhost:3000/passwordreset/${resetToken}`;
+
+        const message = `
+            <h1> You have requested a password reset </h1>
+            <p> Please go the following link to reset your password </p>
+            <a href=${resetURL} clicktracking=off>${resetURL} </a>
+        `
+
+        try {
+            await sendEmail({
+                to: user.email,
+                subject: "Password reset request",
+                text: message
+            });
+
+            res.status(200).json({success: true, data: "Email Sent"});
+
+        } catch (error) {
+            user.resetPasswordToken = undefined;
+            user.resetPasswordExpire = undefined;
+
+            await user.save();
+
+            return next(new ErrorResponse("Email could not be sent", 500))
+
+        }
+    } catch (error) {
+        next(error);
 
 
-        if(!user){
-            return next (new ErrorResponse("invalid credentials",404));
-            }
-    //running against encrypted password, and will return true or false statement
-    const isMatch = await user.matchPasswords(password); 
-
-
-    if (!isMatch) {
-        return next (new ErrorResponse("invalid credentials",404));
     }
+}
 
-
-    sendToken(user, 200, res);
-
-    } catch (error){
-        res.status(500).json ({success: false, error: error.message});
-    }
-};
-
-
-exports.forgotpassword =  (req, res, next) => {
-
-    res.send("Forgot Password Route");
-
-};
-
-exports.resetpassword =  (req, res, next) => {
-
+exports.resetpassword = (req, res, next) => {
     res.send("Reset Password Route");
-
 };
 
-//create function that gets access to user
 const sendToken = (user, statusCode, res) => {
-    //get into the user model
-    const token = user.getSignedJwtToken();
-    res.status(statusCode).json({success:true, token});
-
+    const token = user.getSignedToken();
+    res.status(statusCode).json({ success: true, token})
 }
