679b2c95c86c5fda4b5fc898777441e74c7a0502
L-Sherratt <62371728+L-Sherratt@users.noreply.github.com>
2021-10-02 17:23:54 +1000

    Update ForgotPasswordScreen.js

--- LoginSys/client/src/components/screens/ForgotPasswordScreen.js
+++ LoginSys/client/src/components/screens/ForgotPasswordScreen.js
@@ -38,7 +38,7 @@ const ForgotPasswordScreen = () => {
     <div className = "nav"> 
 
 <ul className="nav ul"> 
-        <Link to="/"><button className='navbar-btn'>Home</button></Link>
+        <Link to="/home"><button className='navbar-btn'>Home</button></Link>
         <Link to="/Register"><button className="navbar-btn">Register</button></Link>
         <Link to="/Login"><button className="navbar-btn">Login</button></Link>
         <button className="navbar-btn">Menu</button>
