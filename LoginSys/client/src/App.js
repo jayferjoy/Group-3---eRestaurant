033b4c7fb053f5ccf245d4ae5215d1aa00c84166
L-Sherratt <62371728+L-Sherratt@users.noreply.github.com>
2021-10-04 10:57:00 +1100

    add manager + staff code

--- LoginSys/client/src/App.js
+++ LoginSys/client/src/App.js
@@ -6,7 +6,9 @@ import PrivateScreen from './components/screens/PrivateScreen';
 import LoginScreen from './components/screens/LoginScreen';
 import EmployeeLoginScreen from './components/screens/EmployeeLoginScreen';
 import EmployeePageScreen from './components/screens/EmployeePageScreen';
+import ManagerPageScreen from './components/screens/ManagerPageScreen';
 import RegisterScreen from './components/screens/RegisterScreen';
+import AddStaffScreen from './components/screens/AddStaffScreen';
 import ForgotPasswordScreen from './components/screens/ForgotPasswordScreen';
 import ResetPasswordScreen from './components/screens/ResetPasswordScreen';
 import CustomerAccountScreen from './components/screens/CustomerAccountScreen';
@@ -22,8 +24,10 @@ const App = () => {
           <Switch>
             <PrivateRoute exact path="/" component={PrivateScreen}/>
             <Route exact path="/login" component={LoginScreen}/>
+            <Route exact path="/addstaff" component={AddStaffScreen}/>
             <Route exact path="/employeelogin" component={EmployeeLoginScreen}/>
             <Route exact path="/employeepage" component={EmployeePageScreen}/>
+            <Route exact path="/managerpage" component={ManagerPageScreen}/>
             <Route exact path="/register" component={RegisterScreen}/>
             <Route exact path="/forgotpassword" component={ForgotPasswordScreen}/>
             <Route exact path="/passwordreset/:resetToken" component={ResetPasswordScreen}/>
