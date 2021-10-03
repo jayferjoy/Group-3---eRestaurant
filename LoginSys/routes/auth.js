a9f8804aedd4a89156b7201e504d52a50883ae74
L-Sherratt <62371728+L-Sherratt@users.noreply.github.com>
2021-10-04 10:53:24 +1100

    update for staff / manager

--- LoginSys/routes/auth.js
+++ LoginSys/routes/auth.js
@@ -2,11 +2,17 @@ const express = require ('express');
 const router = express.Router();
 
 
-const {register, login, forgotpassword, resetpassword} = require ('../controllers/auth');
+const {register, login, forgotpassword, resetpassword, Elogin, Eregister, Mlogin, Mregister} = require ('../controllers/auth');
 
 router.route("/register").post(register);
 
+router.route("/Eregister").post(Eregister);
+router.route("/Mregister").post(Mregister);
+
 router.route("/login").post(login);
+
+router.route("/Elogin").post(Elogin);
+router.route("/Mlogin").post(Mlogin);
 
 router.route("/forgotpassword").post(forgotpassword);
 
