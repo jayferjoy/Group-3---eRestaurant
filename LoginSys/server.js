478bc25d1950fc92366d33e3bff9572d5066191c
jayferjoy <jayfer.development@gmail.com>
2021-10-05 23:48:57 +1100

    override

--- LoginSys/server.js
+++ LoginSys/server.js
@@ -1,29 +1,26 @@
-require("dotenv").config({path: "./config.env"});
-const express = require('express');
-const connectDB = require('./config/db');
-const errorHandler = require('./middleware/error');
+require("dotenv").config({ path: "./config.env" });
+const express = require("express");
+const app = express();
+const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
 
-//connect the mongodb
 connectDB();
 
-
-const app = express ();
-
-//get data from body
 app.use(express.json());
 
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/private", require("./routes/private"));
 
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/private', require('./routes/private'))
 
-
-//Error handler
 app.use(errorHandler);
+
+
+
 
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Sever running on port ${PORT}`)
 );
 
 process.on("unhandledRejection", (err, promise) => {
